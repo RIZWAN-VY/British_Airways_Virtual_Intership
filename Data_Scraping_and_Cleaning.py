6babf1a41c939a316f7886fa0de43f95c6f07fbf
rizwanyoosaf37@gmail.com <rizwanyoosaf37@gmail.com>
2023-09-16 21:09:23 +0530

    parse content

--- Data_Scraping_and_Cleaning.py
+++ Data_Scraping_and_Cleaning.py
@@ -7,3 +7,21 @@ base_url = "https://www.airlinequality.com/airline-reviews/british-airways"
 pages = 30
 page_size = 100
 
+review = []
+review_text = []
+review_rating = []
+recommend = []
+
+for i in range(1, pages + 1):
+
+    print(f"Scraping page {i}")
+
+    # Create URL to collect links from paginated data
+    url = f"{base_url}/page/{i}/?sortby=post_date%3ADesc&pagesize={page_size}"
+
+    # Collect HTML data from this page
+    response = requests.get(url)
+
+    # Parse content
+    content = response.content
+    soup = BeautifulSoup(content, 'html.parser')
